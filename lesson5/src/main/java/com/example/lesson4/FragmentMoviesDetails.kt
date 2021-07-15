5fd630ec0e4abcbd01ee2ff7c8bc9a0b85edfc6d
eels911 <sabinadima1>
2021-07-15 16:58:47 +0300

    lesson5: init

---
+++ lesson5/src/main/java/com/example/lesson4/FragmentMoviesDetails.kt
@@ -0,0 +1,52 @@
+package com.example.lesson4
+
+import android.os.Bundle
+import android.view.View
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class FragmentMoviesDetails() : Fragment(R.layout.fragment_movies_details) {
+    private val tvAge: TextView by lazy { requireView().findViewById<TextView>(R.id.tv_back) }
+    private val tvFilm: TextView by lazy { requireView().findViewById<TextView>(R.id.tv_movie_name) }
+    private val tvGenre: TextView by lazy { requireView().findViewById<TextView>(R.id.tv_movie_tags) }
+    private val rating: RatingBar by lazy { requireView().findViewById<RatingBar>(R.id.rating) }
+    private val tvNumReviews: TextView by lazy { requireView().findViewById<TextView>(R.id.tv_movie_reviews_count) }
+    private val tvStoryLne: TextView by lazy { requireView().findViewById<TextView>(R.id.tv_movie_storyline) }
+    private val actorsAdapter: ActorListAdapter by lazy {
+        ActorListAdapter(requireContext()).apply {
+            setList(generateActors())
+        }
+    }
+
+    private val rvActors: RecyclerView by lazy {
+        requireView().findViewById<RecyclerView>(R.id.rv_actor).apply {
+
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+        }
+    }
+
+    private val tvBack: TextView by lazy { requireView().findViewById<TextView>(R.id.tv_back) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvBack.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+        rvActors.adapter = actorsAdapter
+        val movie = arguments?.getSerializable(EXTRA_MOVIE_DETAIL) as? Movie
+        movie?.run {
+            tvAge.text = age
+            tvFilm.text = name
+            tvGenre.text = genres
+            tvNumReviews.text = numReviews.toString()
+            tvStoryLne.text=storyLine
+
+        }
+
+    }
+}
---
+++ lesson5/src/main/java/com/example/lesson4/Movie.kt
@@ -0,0 +1,14 @@
+package com.example.lesson4
+
+import java.io.Serializable
+
+data class Movie(
+    val name: String,
+    val age: String,
+    val storyLine: String,
+    val rating: Float,
+    val numReviews: Int,
+    val genres: String,
+    val duration: Int,
+    val isFavorite: Boolean,
+    val image: Int): Serializable
